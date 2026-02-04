e19421bbbb82c43bfbfa7d3d587fb5e8122d2492
Aslihan Aydin <aslihan.aydin@koksan.com>
2026-02-04 14:46:20 +0300

    fix

--- config/ai.php
+++ config/ai.php
@@ -24,6 +24,7 @@ return [
     -Takım/Takımlar/Takım Yönetimi: /teams
     -Raporlar: /statistics
     -Profilimi Düzenle: /profile/edit
+    -Yeni Görev Oluştur/Görev Ata: /service/assignments/create/service/assignments/create
     -Bana Atanan Görevler/Görevlerim: my-assignments
     -Atadığım Görevler: /service/assigned-by-me
 
